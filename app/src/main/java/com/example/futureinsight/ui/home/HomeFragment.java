347cff6b5319878bdf14ac5935fce4302778255d
rafay99-epic <99marafaY@gmail.com>
2023-07-17 18:58:34 +0500

    Layout Complete, Working on Java Logic

--- app/src/main/java/com/example/futureinsight/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/futureinsight/ui/home/HomeFragment.java
@@ -1,10 +1,14 @@
 package com.example.futureinsight.ui.home;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -22,8 +26,46 @@ private FragmentHomeBinding binding;
     binding = FragmentHomeBinding.inflate(inflater, container, false);
     View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        String future_insight_home= "https://future-insight.blog/";
+        final WebView homeWebview = binding.idWebViewHome;
+        final ProgressBar loadingPb = binding.PBLoading;
+
+        homeWebview.loadUrl(future_insight_home);
+
+        homeWebview.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                loadingPb.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                loadingPb.setVisibility(View.GONE);
+            }
+        });
+
+        homeWebview.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int keycode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN)
+                {
+                    switch (keycode)
+                        {
+                            case KeyEvent.KEYCODE_BACK:
+                                if (homeWebview.canGoBack())
+                                {
+                                    homeWebview.goBack();
+                                }
+                                break;
+                        }
+                }
+                return false;
+            }
+        });
+
+
         return root;
     }
 
