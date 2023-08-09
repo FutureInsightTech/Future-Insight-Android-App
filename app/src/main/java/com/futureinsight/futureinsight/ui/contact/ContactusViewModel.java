dcb6f8856c6307d396922bba39af1a10b605de72
Abdul Rafay <82662797+rafay99-epic@users.noreply.github.com>
2023-08-10 03:48:18 +0500

    Version 03: Contact Page in Nav Bar

---
+++ app/src/main/java/com/futureinsight/futureinsight/ui/contact/ContactusViewModel.java
@@ -0,0 +1,7 @@
+package com.futureinsight.futureinsight.ui.contact;
+
+import androidx.lifecycle.ViewModel;
+
+public class ContactusViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
