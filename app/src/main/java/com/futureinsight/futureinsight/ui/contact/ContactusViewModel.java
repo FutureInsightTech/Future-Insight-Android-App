348f86e2c5dc0eba22f4b8085be75107571f74ae
rafay99-epic <99marafay@gmail.com>
2023-08-23 16:49:05 +0000

    Code Cleaning & Removing uncesscary code

--- app/src/main/java/com/futureinsight/futureinsight/ui/contact/ContactusViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.futureinsight.futureinsight.ui.contact;
-
-import androidx.lifecycle.ViewModel;
-
-public class ContactusViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/com/futureinsight/futureinsight/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/futureinsight/futureinsight/ui/gallery/GalleryFragment.java
@@ -22,8 +22,6 @@ private FragmentGalleryBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
-        GalleryViewModel galleryViewModel =
-                new ViewModelProvider(this).get(GalleryViewModel.class);
 
     binding = FragmentGalleryBinding.inflate(inflater, container, false);
     View root = binding.getRoot();
